cf13fa821cba052a662a789488f380331c93ac63
Muhammad Khizer Sajjad <mkhizersajjad@gmail.com>
2023-08-10 09:20:52 +0500

    Config thr theme for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,68 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    
+    {{-- <div id="loading">
+        <div id="loading-center">
         </div>
+    </div> --}}
+    
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <div class="wrapper">
+        <section class="login-content">
+            <div class="container h-100">
+                <div class="row align-items-center justify-content-center h-100">
+                    <div class="col-12">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6">
+                                <h2 class="mb-2">Sign In</h2>
+                                <p>To Keep connected with us please login with your personal info.</p>
+                                <form>
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="floating-label form-group">
+                                                <input class="floating-input form-control" type="email"
+                                                    placeholder=" ">
+                                                <label>Email</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="floating-label form-group">
+                                                <input class="floating-input form-control" type="password"
+                                                    placeholder=" ">
+                                                <label>Password</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="custom-control custom-checkbox mb-3">
+                                                <input type="checkbox" class="custom-control-input" id="remember_me" name="remember">
+                                                <label class="custom-control-label" for="remember_me">{{ __('Remember me') }}</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 rtl-left">
+                                            @if (Route::has('password.request'))
+                                                <a href="{{ route('password.request') }}" class="text-primary float-right">{{ __('Forgot your password?') }}</a>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">{{ __('Log in') }}</button>
+                                    <p class="mt-3">
+                                        Create an Account <a href="auth-sign-up.html" class="text-primary">{{ __('Sign Up') }}</a>
+                                    </p>
+                                </form>
+                            </div>
+                            <div class="col-lg-6 mb-lg-0 mb-4 mt-lg-0 mt-4">
+                                <img src="{{ asset('admin/images/login/01.png') }}" class="img-fluid w-80"
+                                    alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    {{-- <div class="custom-control-inline change-rtl">
+        <a href="#" class="switch-rtl" data-active="true" for="rtl-mode" data-mode="rtl">RTL</a>
+    </div> --}}
 </x-guest-layout>
