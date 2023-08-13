4a77268e8e086bf3471542ef33d3a9604ec68cf7
Muhammad Khizer Sajjad <mkhizersajjad@gmail.com>
2023-08-13 05:55:11 +0500

    add header and footer

---
+++ resources/views/admin/common/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="iq-footer rtl-footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 ">
+                <ul class="list-inline mb-0  rtl-pr-0 rtl-right">
+                    <li class="list-inline-item "><a href="privacy-policy.html">Privacy Policy</a></li>
+                    <li class="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-6 text-right ">
+                Copyright @php date('Y') @endphp <a href="#">Rights Association</a> All Rights Reserved.
+            </div>
+        </div>
+    </div>
+</footer>
