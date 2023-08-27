a40870cbaeecd553a1afffa88149c8a7da771f03
Muhammad Khizer Sajjad <mkhizersajjad@gmail.com>
2023-08-27 15:34:48 +0500

    update helpers for appointment

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -44,10 +44,27 @@
         $statuses = [
             '1' => ['Requested', '<span class="badge bg-info-light">Requested</span>'],
             '2' => ['Scheduled', '<span class="badge bg-primary-light">Scheduled</span>'],
-            '3' => ['Process start', '<span class="badge bg-info-light">Process start</span>'],
-            '4' => ['Under observation', '<span class="badge bg-secondary-light">Under observation</span>'],
-            '5' => ['Negotiating with 3rd party', '<span class="badge bg-info-light">Negotiating with 3rd party</span>'],
-            '6' => ['Waiting for customer response', '<span class="badge bg-warning-light">Waiting for customer response</span>'],
+            '3' => ['Case Proceed', '<span class="badge bg-success-light">Case Proceed</span>'],
+            '4' => ['Rescheduled', '<span class="badge bg-primary-light">Rescheduled</span>'],
+            '5' => ['Pending', '<span class="badge bg-warning-light">Pending</span>'],
+            '6' => ['Withdrawed', '<span class="badge bg-success-light">Withdrawed</span>'],
+            '7' => ['Rejected', '<span class="badge bg-danger-light">Rejected</span>'],
+        ];
+
+        return 
+            isset($statuses[$status])
+            ? ($type === 'badge' ? $statuses[$status][1] : $statuses[$status][0])
+            : ($type === 'badge' ? array_column($statuses, 1) : array_column($statuses, 0));
+    }
+    
+    function getCaseStatus($status = null, $type = null)
+    {
+        $statuses = [
+            '1' => ['Process Start', '<span class="badge bg-info-light">Process Start</span>'],
+            '2' => ['Under observation', '<span class="badge bg-secondary-light">Under observation</span>'],
+            '3' => ['Negotiating', '<span class="badge bg-info-light">Negotiating</span>'],
+            '4' => ['Waiting for customer response', '<span class="badge bg-warning-light">Waiting for customer response</span>'],
+            '6' => ['Waiting for 3rd party response', '<span class="badge bg-warning-light">Waiting for 3rd party response</span>'],
             '7' => ['Suspended', '<span class="badge bg-danger-light">Suspended</span>'],
             '8' => ['Resolved', '<span class="badge bg-success-light">Resolved</span>'],
         ];
