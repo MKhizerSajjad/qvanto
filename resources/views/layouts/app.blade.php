6e579091b4c06181bac474b78cca5d41943d142c
Muhammad Khizer Sajjad <mkhizersajjad@gmail.com>
2023-09-05 20:28:19 +0500

    add charts cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,8 @@
         <link rel='stylesheet' href="{{ asset('admin/vendor/fullcalendar/list/main.css') }}"/>
         <link rel="stylesheet" href="{{ asset('admin/vendor/mapbox/mapbox-gl.css') }}">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
 
     </head>
     <body class="font-sans antialiased">
